16e748c262c2ccae97cb80486ad42d97320e63f1
Amoor <amoor6navasartian@gmail.com>
2025-12-02 19:46:21 +0330

    added header

---
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/components/main-header.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import logoImg from '@/assets/logo.png';
+export default function MainHeader() {
+  return (
+    <header>
+      <Link href='/'>
+        <img src={logoImg.src} alt='A food app' />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/meals'>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href='/community'>Foodies community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
