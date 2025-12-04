676b7155be002068ebac4db11b24a07bde6a09c5
Amoor <amoor6navasartian@gmail.com>
2025-12-04 20:44:18 +0330

    added path

--- code/02-nextjs-essentials/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -3,26 +3,27 @@ import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-links';
 export default function MainHeader() {
   return (
     <>
-    <MainHeaderBackground />
-    <header className={classes.header}>
-      <Link className={classes.logo} href='/'>
-        <Image src={logoImg} alt='A food app' priority/>
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Foodies community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          <Image src={logoImg} alt='A food app' priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href='/meals'>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href='/community'>Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
