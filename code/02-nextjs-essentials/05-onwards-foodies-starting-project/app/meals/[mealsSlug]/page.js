8145903b1ea8120aa6c6c5a70e22096a54aa1734
Amoor <amoor6navasartian@gmail.com>
2025-12-15 19:08:23 +0330

    meals slug added

--- code/02-nextjs-essentials/05-onwards-foodies-starting-project/app/meals/[mealsSlug]/page.js
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/app/meals/[mealsSlug]/page.js
@@ -1,9 +1,32 @@
-function page({ params }) {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+async function MealDetailPage({ params }) {
+  const meal = getMeal(params.mealsSlug);
   return (
-    <div>
-      <h1>Meal: {params.slug}</h1>
-    </div>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions || '',
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
-export default page
+export default MealDetailPage;
--- code/02-nextjs-essentials/05-onwards-foodies-starting-project/lib/meals.js
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/lib/meals.js
@@ -1,9 +1,13 @@
-import sql from 'better-sqlite3'
+import sql from 'better-sqlite3';
 
-const db = sql('meals.db')
+const db = sql('meals.db');
 
 export async function getMeals() {
-    await new Promise((resolve) => setTimeout(resolve, 5000))
-    // throw new Error('loading meals failed')
-  return db.prepare('SELECT * FROM meals').all()
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error('loading meals failed')
+  return db.prepare('SELECT * FROM meals').all();
+}
+
+export  function getMeal(slug) {
+  return db.prepare('SELECT * from meals WHERE slug = ?').get(slug);
 }
